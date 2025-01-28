9f76463873bd41a26a7944c65e7935ad9c31b661
RAMANA <ramanabarani2510@gmail.com>
2025-01-28 22:48:11 +0530

    Delete static directory

--- static/js/login.js
+++
@@ -1,9 +0,0 @@
-function signup() {
-    document.querySelector(".login-form-container").style.display = "none";
-    document.querySelector(".signup-form-container").style.display = "block";
-}
-
-function login() {
-    document.querySelector(".signup-form-container").style.display = "none";
-    document.querySelector(".login-form-container").style.display = "block";
-}
